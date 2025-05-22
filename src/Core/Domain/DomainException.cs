49e5245d9f71be8b7fcede97e6d034b6b57edddf
tuliorezende <tulior@blip.ai>
2025-05-22 20:59:38 -0300

    Adjust exception implementation to use an override message parameter.

--- src/Core/Domain/DomainException.cs
+++ src/Core/Domain/DomainException.cs
@@ -2,13 +2,4 @@ namespace Domain;
 
 public class DomainException : Exception
 {
-    public DomainException()
-    {
-        
-    }
-    
-    internal DomainException(string businessMessage)
-        : base(businessMessage)
-    {
-    }
 }
