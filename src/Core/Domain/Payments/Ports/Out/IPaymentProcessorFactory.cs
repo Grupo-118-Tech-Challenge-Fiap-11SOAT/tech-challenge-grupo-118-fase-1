54777ce0752a3eaa7b1e5e1019af410157ca70c3
Vinicius Rossmann Nunes <viniciusrossmann@gmail.com>
2025-05-24 00:40:33 -0300

    add method xml comments

--- src/Core/Domain/Payments/Ports/Out/IPaymentProcessorFactory.cs
+++ src/Core/Domain/Payments/Ports/Out/IPaymentProcessorFactory.cs
@@ -4,5 +4,10 @@ namespace Domain.Payments.Ports.Out;
 
 public interface IPaymentProcessorFactory
 {
+    /// <summary>
+    /// Returns the payment processor for the specified payment provider.
+    /// </summary>
+    /// <param name="provider">The payment provider/param>
+    /// <returns>The payment provider processor</returns>
     IPaymentProcessor GetProcessor(PaymentProvider provider);
 }
