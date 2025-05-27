82a83756d0d5457059ffe6a826681af04e0f7566
Vinicius Rossmann Nunes <viniciusrossmann@gmail.com>
2025-05-27 00:29:00 -0300

    update snapshot

--- src/Adapters/Driven/Infra.Database.SqlServer/Migrations/20250524033103_CreatePaymentTable.Designer.cs
+++ src/Adapters/Driven/Infra.Database.SqlServer/Migrations/20250524033103_CreatePaymentTable.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infra.Database.SqlServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250527030125_CreatePaymentTable")]
+    [Migration("20250524033103_CreatePaymentTable")]
     partial class CreatePaymentTable
     {
         /// <inheritdoc />
@@ -135,7 +135,7 @@ namespace Infra.Database.SqlServer.Migrations
                     b.HasIndex("Uuid")
                         .IsUnique();
 
-                    b.ToTable("Payments", (string)null);
+                    b.ToTable("PAYMENT", (string)null);
                 });
 
             modelBuilder.Entity("Infra.Database.SqlServer.Products.Entities.ImageProduct", b =>
