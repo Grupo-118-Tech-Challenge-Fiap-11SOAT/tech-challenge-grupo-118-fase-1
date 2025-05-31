865bb551b800ff6dced8f4de01f75c3f807014c7
tuliorezende <tulior@blip.ai>
2025-05-30 23:24:24 -0300

    change payment table name

---
+++ src/Adapters/Driven/Infra.Database.SqlServer/Migrations/20250531022306_ChangePaymentTableName.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePaymentTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PAYMENT",
+                table: "PAYMENT");
+
+            migrationBuilder.RenameTable(
+                name: "PAYMENT",
+                newName: "Payments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PAYMENT_Uuid",
+                table: "Payments",
+                newName: "IX_Payments_Uuid");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments");
+
+            migrationBuilder.RenameTable(
+                name: "Payments",
+                newName: "PAYMENT");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payments_Uuid",
+                table: "PAYMENT",
+                newName: "IX_PAYMENT_Uuid");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PAYMENT",
+                table: "PAYMENT",
+                column: "Id");
+        }
+    }
+}
