25eee2c79e56011b6aa09e64ac4d0c321078081a
TiagoVictor <tiago.oliveira@midassolutions.com.br>
2025-06-01 18:56:08 -0300

    feat: atualizando customer

--- src/Adapters/Driving/TechChallengeFastFood.API/Controllers/CustomerController.cs
+++ src/Adapters/Driving/TechChallengeFastFood.API/Controllers/CustomerController.cs
@@ -12,6 +12,13 @@ namespace TechChallengeFastFood.API.Controllers
     {
         private readonly ICustomerManager _customerManager;
 
+        private readonly ProblemDetails CUSTOMER_NOT_FOUND = new ProblemDetails
+        {
+            Title = "Customer not found",
+            Status = StatusCodes.Status404NotFound,
+            Detail = "The requested customer could not be found."
+        };
+
         /// <summary>
         /// Customer constructor
         /// </summary>
@@ -21,64 +28,69 @@ namespace TechChallengeFastFood.API.Controllers
         /// <summary>
         /// Update a Customer
         /// </summary>
-        /// <param name="customerDto"></param>
+        /// <param name="customerUpdateDto"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(CustomerResponseDto), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails),StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [HttpPut]
-        public async Task<ActionResult<CustomerDto>> PutAsync([FromBody] CustomerDto customerDto, CancellationToken cancellationToken)
+        public async Task<ActionResult<CustomerResponseDto>> PutAsync([FromBody] CustomerUpdateDto customerUpdateDto, CancellationToken cancellationToken)
         {
-            var result = await _customerManager.UpdateAsync(customerDto, cancellationToken);
+            var result = await _customerManager.UpdateAsync(customerUpdateDto, cancellationToken);
 
             return result.Error ? BadRequest(result) : Ok(result);
         }
         /// <summary>
         /// Create a new Customer
         /// </summary>
-        /// <param name="customerDto"></param>
+        /// <param name="custormerRequestDto"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(CustomerResponseDto), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails),StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [HttpPost]
-        public async Task<ActionResult<CustomerDto>> PostAsync([FromBody] CustomerDto customerDto, CancellationToken cancellationToken)
+        public async Task<ActionResult<CustomerResponseDto>> PostAsync([FromBody] CustomerRequestDto custormerRequestDto, CancellationToken cancellationToken)
         {
-            var result = await _customerManager.CreateAsync(customerDto, cancellationToken);
+            var result = await _customerManager.CreateAsync(custormerRequestDto, cancellationToken);
 
             return result.Error ? BadRequest(result) : Ok(result);
         }
-        
+
         /// <summary>
         /// Return a Customer filtering by Id
         /// </summary>
         /// <param name="id"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(CustomerResponseDto), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails),StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerDto>> GetByIdAsync(int id, CancellationToken cancellationToken)
+        public async Task<ActionResult<CustomerResponseDto>> GetByIdAsync(int id, CancellationToken cancellationToken)
         {
             var customer = await _customerManager.GetByIdAsync(id, cancellationToken);
 
-            return customer is null ? NotFound() : Ok(customer);
+            if (customer == null)
+            {
+                return NotFound(CUSTOMER_NOT_FOUND);
+            }
+
+            return Ok(customer);
         }
-        
+
         /// <summary>
         /// Return a Customer filtering by CPF
         /// </summary>
         /// <param name="cpf"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(CustomerResponseDto), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails),StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [HttpGet("/cpf/{cpf}")]
-        public async Task<ActionResult<CustomerDto>> GetByIdAsync(string cpf, CancellationToken cancellationToken)
+        public async Task<ActionResult<CustomerResponseDto>> GetByIdAsync(string cpf, CancellationToken cancellationToken)
         {
             var customer = await _customerManager.GetByCpfAsync(cpf, cancellationToken);
 
